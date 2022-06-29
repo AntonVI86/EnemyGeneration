0f56c3491fba5007daa0aa4d5acf074fa3c62fca
AntonVI86 <lionter0@gmail.com>
2022-06-29 14:08:42 +0300

    Init

---
+++ EnemyGeneration/Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _damage;
+    [SerializeField] private ParticleSystem _hitEffect;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.up * -_speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out EnemyHealth enemyHealth)) 
+        {
+            enemyHealth.TakeDamage(_damage);
+            Instantiate(_hitEffect, transform.position, Quaternion.identity);
+        }
+
+        gameObject.SetActive(false);
+    }
+}
