a9634f5f8f09301f8a2d6cc80fa48c5fb4de54e8
AntonVI86 <lionter0@gmail.com>
2022-06-29 14:21:03 +0300

    Init

---
+++ Assets/Scripts/EnemyAudioSource.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class EnemyAudioSource : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();       
+    }
+
+    public void PlayClip(AudioClip clip) 
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+
+    public void PlayClip(AudioClip[] clips)
+    {
+        int clipIndex = Random.Range(0, clips.Length);
+
+        _audioSource.PlayOneShot(clips[clipIndex]);
+    }
+}
---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private float _maxArmorPoints;
+    [SerializeField] private AudioClip[] _deathSound;
+    [SerializeField] private AudioClip[] _hitSound;
+    [SerializeField] private ParticleSystem _destroyEffect;
+    
+    private EnemyAudioSource _audioSource;
+    private float _armorPoint;
+
+    private void Awake()
+    {
+        _audioSource = FindObjectOfType<EnemyAudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        _armorPoint = _maxArmorPoints;
+    }
+
+    public void TakeDamage(float damage) 
+    {
+        _armorPoint -= damage;
+        _audioSource.PlayClip(_hitSound);
+
+        if (_armorPoint <= 0) 
+        {
+            _audioSource.PlayClip(_deathSound);
+            Instantiate(_destroyEffect, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+    }
+}
